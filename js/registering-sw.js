0a39156ea03fcfe12a39ab6c5eeaae5c5ede5263
MobileWebSpecialistNanodegreeWithGoogleatUdacity <34831980+Iamsubhansultan@users.noreply.github.com>
2018-09-18 18:20:21 +0530

    Add files via upload

---
+++ js/registering-sw.js
@@ -0,0 +1,8 @@
+navigator.serviceWorker.register('/sw.js').then(function() {
+	if (!navigator.serviceWorker.controller) {
+		return;
+	}
+console.log('service worker registered');
+}).catch(function() {
+	console.log('service worker registration failed');
+});
